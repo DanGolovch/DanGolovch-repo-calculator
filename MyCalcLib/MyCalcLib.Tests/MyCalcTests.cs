835d24a04f4ca3dc8cf07d42d28b1700082df60a
DanGolovch <114566306+DanGolovch@users.noreply.github.com>
2022-10-24 21:14:12 +0300

    Add files via upload

---
+++ MyCalcLib/MyCalcLib.Tests/MyCalcTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace MyCalcLib.Tests
+{
+    public class MyCalcTests
+    {
+        [Fact]
+        public void Sum_10and20_30returned()
+        {
+            // arrange 
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+
+            // act
+            MyCalc c = new MyCalc();
+            int actual = c.Sum(x, y);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
